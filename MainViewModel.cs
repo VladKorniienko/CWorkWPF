ea054265d14defecd4064bed79b30de571d937dc
Vladyslav Korniienko <Vladyslav_Korniienko@epam.com>
2020-06-17 02:44:18 +0300

    Add average results

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using CWork.Entities.Input;
 using CWork.Solvers;
 using OxyPlot;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace CWork
 {
@@ -10,15 +12,19 @@ namespace CWork
     {
         public MainViewModel()
         {
-            this.Title = "Red - Branch&Bound algorithm\nGreen - Randomized algorithm\nPurple - Greedy algoritm";
+
             this.timeResultsBaB1000 = new List<DataPoint>();
             this.timeResultsRand1000 = new List<DataPoint>();
             this.timeResultsGreedy1000 = new List<DataPoint>();
+            this.timeResultsDynamic1000 = new List<DataPoint>();
             this.timeResultsBaB = new List<DataPoint>();
             this.timeResultsRand = new List<DataPoint>();
             this.timeResultsGreedy = new List<DataPoint>();
+            this.timeResultsDynamic = new List<DataPoint>();
             SolveMultiple1000();
             SolveMultiple100();
+            this.Title = "Red - Branch&Bound algorithm\nGreen - Randomized algorithm\nPurple - Greedy algorithm\nOrange - Dynamic algorithm\nAverage: G-"
+                + timeAverageGreedy + " D-" + timeAverageDynamic + " R-" + timeAverageRand + " B&B-" + timeAverageBaB;
         }
         private void SolveMultiple1000()
         {
@@ -31,6 +37,7 @@ namespace CWork
                 var solverBaB = new BranchAndBoundSolver(input.Items, input.Capacity);
                 var solverRand = new RandomSolver(input.Items, input.Capacity);
                 var solverGreedy = new GreedySolver(input.Items, input.Capacity);
+                var solverDynamic = new DynamicProgrammingSolver(input.Items, input.Capacity);
                 stopwatch.Start();
                 solverBaB.Solve();
                 stopwatch.Stop();
@@ -43,7 +50,15 @@ namespace CWork
                 solverGreedy.Solve();
                 stopwatch.Stop();
                 timeResultsGreedy1000.Add(new DataPoint(i, stopwatch.Elapsed.TotalMilliseconds));
+                stopwatch.Restart();
+                solverDynamic.Solve();
+                stopwatch.Stop();
+                timeResultsDynamic1000.Add(new DataPoint(i, stopwatch.Elapsed.TotalMilliseconds));
             }
+            timeAverageGreedy = Math.Round((timeResultsGreedy1000.Sum(x => x.Y) / 1000), 4);
+            timeAverageRand = Math.Round((timeResultsRand1000.Sum(x => x.Y) / 1000), 4);
+            timeAverageBaB = Math.Round((timeResultsBaB1000.Sum(x => x.Y) / 1000), 4);
+            timeAverageDynamic = Math.Round((timeResultsDynamic1000.Sum(x => x.Y) / 1000), 4);
         }
         private void SolveMultiple100()
         {
@@ -56,6 +71,7 @@ namespace CWork
                 var solverBaB = new BranchAndBoundSolver(input.Items, input.Capacity);
                 var solverRand = new RandomSolver(input.Items, input.Capacity);
                 var solverGreedy = new GreedySolver(input.Items, input.Capacity);
+                var solverDynamic = new DynamicProgrammingSolver(input.Items, input.Capacity);
                 stopwatch.Start();
                 solverBaB.Solve();
                 stopwatch.Stop();
@@ -68,15 +84,29 @@ namespace CWork
                 solverGreedy.Solve();
                 stopwatch.Stop();
                 timeResultsGreedy.Add(new DataPoint(i, stopwatch.Elapsed.TotalMilliseconds));
+                stopwatch.Restart();
+                solverDynamic.Solve();
+                stopwatch.Stop();
+                timeResultsDynamic.Add(new DataPoint(i, stopwatch.Elapsed.TotalMilliseconds));
             }
+            timeAverageGreedy = Math.Round((timeResultsGreedy.Sum(x => x.Y) / 100), 4);
+            timeAverageRand = Math.Round((timeResultsRand.Sum(x => x.Y) / 100), 4);
+            timeAverageBaB = Math.Round((timeResultsBaB.Sum(x => x.Y) / 100), 4);
+            timeAverageDynamic = Math.Round((timeResultsDynamic.Sum(x => x.Y) / 100), 4);
         }
         public List<DataPoint> timeResultsBaB1000 { get; set; }
 
         public List<DataPoint> timeResultsRand1000 { get; set; }
         public List<DataPoint> timeResultsGreedy1000 { get; set; }
+        public List<DataPoint> timeResultsDynamic1000 { get; set; }
+        public List<DataPoint> timeResultsDynamic { get; set; }
         public List<DataPoint> timeResultsBaB { get; set; }
         public List<DataPoint> timeResultsRand { get; set; }
         public List<DataPoint> timeResultsGreedy { get; set; }
+        public double timeAverageRand { get; set; }
+        public double timeAverageBaB { get; set; }
+        public double timeAverageDynamic { get; set; }
+        public double timeAverageGreedy { get; set; }
         public PlotModel MyModel { get; private set; }
         public string Title { get; private set; }
     }
